142ae7d5c6b8125d5658318ae8eace4a898cb594
zimkhitha123 <zimkhithambanyaru3@gmail.com>
2025-07-17 11:46:48 +0200

    Added JavaScript

--- src/script.js
+++ src/script.js
@@ -1,1 +1,13 @@
-alert("Hello World");
+function generatePoem(event){
+    event.preventDefault();
+
+new Typewriter('#poem', {
+  strings: "Even so my sun one early morn did shine,With all triumphant splendour on my brow",
+  autoStart: true,
+  delay:1,
+  cursor:"",
+});
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit",generatePoem);
